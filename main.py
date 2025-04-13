46123209a79f7287f54853e6afabf3782b947421
rigofekete <fabricecosteira@gmail.com>
2025-04-13 02:13:17 +0200

    C: player.py added

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # this is the same as below but passing actual RGB values 
         # screen.fill((0,0,0))
         screen.fill("black")
+        player.draw(screen)
+        # flip is basically a blit from the backbuffer to the front buffer
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
