4c955ea3119a078bf0b348d17654b6bf03e261d7
rigofekete <fabricecosteira@gmail.com>
2025-04-14 17:27:43 +0200

    F: updatable/drawable groups implemented

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    # define empty groups 
+    #These are empty to begin with, but they are now ready to hold objects.
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # This sets a static field member in the Player class 
+    # Defines that any instance of the Player class will automatically be added to both the updatable and drawable groups when it's created. 
+    Player.containers = (updatable, drawable)
+    
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
@@ -15,19 +24,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+       
+        updatable.update(dt)
 
-        player.update(dt)
         # this is the same as below but passing actual RGB values 
         # screen.fill((0,0,0))
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         # flip is basically a blit from the backbuffer to the front buffer
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-
 
 
 if __name__ == "__main__":
